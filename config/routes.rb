279aaacf378122d5690d924a1cfdb0a07537a2f7
Matthew Orzechowski <morzechowski@berkeley.edu>
2016-03-03 23:18:02 -0800

    HW4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "users", to: "users#index"
-  get "users/new", to: "users#new"
-  get "users/:id", to: "users#show", as: :user
-  post "users", to: "users#create"
+  resources  :users #automatically finds all HTML verbs and all paths
+  resources  :dogs
 end
